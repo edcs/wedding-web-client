a3362b2f2b9659fb0c044b346ca491cd86c3ebfe
Edward Coleridge Smith <edcoleridgesmith@gmail.com>
2018-02-03 20:09:48 +0000

    Install bugsnag

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,21 @@
+import { AppContainer } from 'react-hot-loader';
+import bugsnag from 'bugsnag-js';
+import bugsnagReact from 'bugsnag-react';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
 
 import App from './App';
 
+const client = bugsnag(process.env.BUGSNAG_API_KEY);
+const ErrorBoundary = client.use(bugsnagReact(React));
 const root = document.getElementById('root');
 
 const render = (Component) => {
   ReactDOM.render(
     <AppContainer>
-      <Component />
+      <ErrorBoundary>
+        <Component />
+      </ErrorBoundary>
     </AppContainer>,
     root,
   );
