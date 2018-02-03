af523a3497e2f9cf31ed860d8d3d074f52eabc9a
Edward Coleridge Smith <edcoleridgesmith@gmail.com>
2018-02-03 10:59:32 +0000

    Made a fancy heading

--- src/components/atoms/Heading2.jsx
+++ src/components/atoms/Heading2.jsx
@@ -1,20 +1,47 @@
+/* eslint-disable indent */
 import PropTypes from 'prop-types';
 import React from 'react';
 import glamorous from 'glamorous';
 
-const H2 = glamorous.h2(`
-  font-serif
-  font-light
-  italic
+const Wrapper = glamorous.div(`
   mt-10
   mb-11
-  text-5xl
+  ml-auto
+  mr-auto
+  relative
   text-center
+`, { maxWidth: 720 });
+
+const H2 = glamorous.h2(`
+  font-serif
+  font-normal
+  inline-block
+  pl-8
+  pr-8
+  relative
+  text-5xl
   tracking-medium
-`);
+  z-20
+`, ({ theme }) => ({
+  backgroundColor: theme.colors.white,
+}));
+
+const Rule = glamorous.div(({ theme }) => ({
+  backgroundColor: theme.colors.teal,
+  height: 3,
+  left: 0,
+  marginTop: 1.5,
+  position: 'absolute',
+  right: 0,
+  top: '50%',
+  zIndex: 10,
+}));
 
 const Heading2 = ({ children }) => (
-  <H2>{children}</H2>
+  <Wrapper>
+    <H2>{children}</H2>
+    <Rule />
+  </Wrapper>
 );
 
 Heading2.propTypes = {
