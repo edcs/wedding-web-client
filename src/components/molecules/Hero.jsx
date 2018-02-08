28e15fc2be535ac8459464ba69d2091ac7ea35a8
Edward Coleridge Smith <edcoleridgesmith@gmail.com>
2018-02-08 18:06:28 +0000

    Use static image rather than SVG for splash

--- src/components/molecules/Hero.jsx
+++ src/components/molecules/Hero.jsx
@@ -1,18 +1,27 @@
+/* eslint-disable indent */
 import React from 'react';
 import glamorous from 'glamorous';
 
 import Names from '../atoms/Names';
-import Splash from '../atoms/Splash';
+import splash from '../../assets/images/splash.png';
 
-const Wrapper = glamorous.div('absolute pin flex justify-center items-center');
+const Wrapper = glamorous.div(`
+  bg-center
+  bg-cover
+  content-center
+  flex
+  justify-center
+`, {
+  width: '100vw',
+  height: '100vh',
+  overflow: 'hidden',
+  backgroundImage: `url(${splash})`,
+});
 
 const Hero = () => (
-  <div>
-    <Wrapper>
-      <Names />
-    </Wrapper>
-    <Splash />
-  </div>
+  <Wrapper>
+    <Names />
+  </Wrapper>
 );
 
 export default Hero;
