3df3c9b69296b121e4631a36b2478aecf2c1d61f
Edward Coleridge Smith <edcoleridgesmith@gmail.com>
2018-02-08 17:00:16 +0000

    Disable logging in development

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,22 @@
 import { AppContainer } from 'react-hot-loader';
 import bugsnag from 'bugsnag-js';
 import bugsnagReact from 'bugsnag-react';
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
 
-const client = bugsnag(process.env.BUGSNAG_API_KEY);
-const ErrorBoundary = client.use(bugsnagReact(React));
+let ErrorBoundary = Fragment;
+
+if (process.env.NODE_ENV === 'production') {
+  const client = bugsnag(process.env.BUGSNAG_API_KEY);
+  ErrorBoundary = client.use(bugsnagReact(React));
+}
+
 const root = document.getElementById('root');
 
 const render = (Component) => {
+
   ReactDOM.render(
     <AppContainer>
       <ErrorBoundary>
