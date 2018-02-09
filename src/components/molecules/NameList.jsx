1cabd123ec16b094daccd505d0f83fc926d7ac47
Edward Coleridge Smith <edcoleridgesmith@gmail.com>
2018-02-09 15:42:00 +0000

    Group names together

--- src/components/molecules/NameList.jsx
+++ src/components/molecules/NameList.jsx
@@ -1,5 +1,6 @@
+/* eslint-disable class-methods-use-this */
 import PropTypes from 'prop-types';
-import React, { Fragment } from 'react';
+import React, { Fragment, PureComponent } from 'react';
 
 import ButtonPrimary from '../atoms/ButtonPrimary';
 import ErrorMessage from '../atoms/ErrorMessage';
@@ -7,28 +8,50 @@ import Heading3 from '../atoms/Heading3';
 import ListItem from '../atoms/ListItem';
 import UnorderedList from '../atoms/UnorderedList';
 
-const NameList = ({ names, onSelect, onSearchAgain }) => (
-  <Fragment>
-    <Heading3>
-      { names.length > 0 ? 'Now choose your invite from the list below:' : null }
-    </Heading3>
-    { names.length > 0 ?
-      <UnorderedList>
-        { names.map(({ inviteId, name }) => (
-          <ListItem onClick={() => onSelect(inviteId)}>{name}</ListItem>
-        ))}
-      </UnorderedList> :
+class NameList extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = { names: this.mapNames(props.names) };
+  }
+
+  mapNames(names) {
+    return names.reduce((accumulator, value) => ({
+      ...accumulator,
+      [value.inviteId]: [...(accumulator[value.inviteId] || []), value.name],
+    }), {});
+  }
+
+  joinNames(names) {
+    return names.join(', ').replace(/,([^,]*)$/, ' & $1');
+  }
+
+  render() {
+    return (
       <Fragment>
-        <ErrorMessage>
-          Sorry, we couldn't find your invitation.
-          <br /><br />
-          <ButtonPrimary type="button" onClick={() => onSearchAgain()}>
-            Search Again?
-          </ButtonPrimary>
-        </ErrorMessage>
-      </Fragment> }
-  </Fragment>
-);
+        <Heading3>
+          { Object.keys(this.state.names).length > 0 ? 'Now choose your invite from the list below:' : null }
+        </Heading3>
+        { Object.keys(this.state.names).length > 0 ?
+          <UnorderedList>
+            { Object.keys(this.state.names).map(inviteId => (
+              <ListItem onClick={() => this.props.onSelect(inviteId)}>
+                {this.joinNames(this.state.names[inviteId])}
+              </ListItem>
+            ))}
+          </UnorderedList> :
+          <Fragment>
+            <ErrorMessage>
+              Sorry, we couldn't find your invitation.
+              <br /><br />
+              <ButtonPrimary type="button" onClick={() => this.props.onSearchAgain()}>
+                Search Again?
+              </ButtonPrimary>
+            </ErrorMessage>
+          </Fragment> }
+      </Fragment>
+    );
+  }
+}
 
 NameList.propTypes = {
   names: PropTypes.array,
